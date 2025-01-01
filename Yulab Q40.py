1526c65702d7cf68b13a46a7219b075b311f7ec9
Hinna0818 <hinna@i.smu.edu.cn>
2025-01-02 00:22:28 +0800

    update_version

--- Yulab Q40.py
+++ Yulab Q40.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def fun():
     a = []
     i = 1
@@ -8,15 +7,4 @@ def fun():
     a = ''.join(a)
     return int(a[0]) * int(a[9]) * int(a[99]) * int(a[999]) * int(a[9999]) * int(a[99999]) * int(a[999999])
 
-=======
-def fun():
-    a = []
-    i = 1
-    while len(a) < 1000000:
-        a.extend(str(i))
-        i += 1
-    a = ''.join(a)
-    return int(a[0]) * int(a[9]) * int(a[99]) * int(a[999]) * int(a[9999]) * int(a[99999]) * int(a[999999])
-
->>>>>>> 3f15ab277c139ecdfe2bf6b799f4f01d1a658c42
 print(fun())
